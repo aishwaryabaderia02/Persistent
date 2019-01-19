25cd2526e4580d4ad13660c9e34561817cbb9061
aishwaryabaderia02 <aishwaryabaderia02@gmail.com>
2019-01-19 15:06:47 +0530

    yo

--- Assignments/src/Day2/Contact.java
+++ Assignments/src/Day2/Contact.java
@@ -1,7 +1,8 @@
 package Day2;
 
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
+import Day2.ContactStack;
 public class Contact {
 	private String fName, lName, dob, gender, anniversary, address, area, city , pin, state, country, telephone, mobile, email, website;
 
@@ -125,21 +126,61 @@ public class Contact {
 		this.website = website;
 	}
 	
-	public static void validate() throws ValueNotFoundException {
-		if(!c.getEmail().isEmpty()){
-			throw new ValueNotFoundException("Value not found");
-			}
-		if(!c.getfName().isEmpty()){
-			throw new ValueNotFoundException("Value not found");
-			}
-		if(!c.getlName().isEmpty()){
-			throw new ValueNotFoundException("Value not found");
-			}
-		if(!c.getDob().isEmpty()){
-			throw new ValueNotFoundException("Value not found");
+	public static void validate(Contact c) throws ValueNotFoundException {
+		
+		//code for empty string
+		if(c.getfName() == null)
+		{
+			System.out.println("first name");
+			throw new ValueNotFoundException("Enter value");
 			}	
+		if(c.getlName() == null)
+		{
+			System.out.println("last name");
+			throw new ValueNotFoundException("Enter value");
+			}	
+		if(c.getDob() == null)
+		{
+			System.out.println("dob");
+			throw new ValueNotFoundException("Enter value");
+			}	
+		if(c.getEmail() == null)
+		{
+			System.out.println("emailnull");
+			throw new ValueNotFoundException("Enter value");
+			}
+		
+		
+			// code for email validation
+			String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                "[a-zA-Z0-9_+&*-]+)*@" + 
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                "A-Z]{2,7}$"; 
+                  
+	/*		Pattern pat = Pattern.compile(emailRegex); 
+			if (c.getEmail() == null) 
+			{	
+				System.out.println("emailNull");
+				throw new ValueNotFoundException("Email error");
+				}
+			
+			if(pat.matcher(c.getEmail()).matches())
+			{
+				System.out.println("email");
+				throw new ValueNotFoundException("Email error");	
+				}
+		*/	
+			
+			//code for mobile or telephone
+			if((c.getMobile() == null) & c.getTelephone()  == null)
+			{
+				System.out.println("mobile");
+				throw new ValueNotFoundException("Enter value");
+				}
+}
+		
 		 
-	}
+	
 	
 	
 	
@@ -147,24 +188,43 @@ public class Contact {
 		Contact c = new Contact();
 		Scanner sc = new Scanner(System.in);
 		
-		c.fName = sc.nextLine(); c.lName = sc.nextLine();
-		c.dob = sc.nextLine(); c.email = sc.nextLine();
-		System.out.println("first name");
-		c.setfName(c.fName);
-		c.setlName(c.lName);
-		c.setDob(c.dob);
-		c.setEmail(c.email);
+		String fName = sc.nextLine(); String lName = sc.nextLine();
+		String dob = sc.nextLine(); String email = sc.nextLine();
+		String mobile = sc.nextLine(); String telephone = sc.nextLine();
+		//System.out.println("first name"); */
+		c.setfName(fName);
+		c.setlName(lName);
+		c.setDob(dob);
+		c.setEmail(email);
+		c.setTelephone(telephone);
+		c.setMobile(mobile);
+		
+		int k = 0;
+		Contact[] stack = new Contact[3];
+		
 		try{
-			c.validate();
+			//System.out.println("try");
+			ContactStack.push(stack, k, c);	
+		//	System.out.println(k);
+			ContactStack.pop(stack, k);
+			//validate(c);
+			
 		}
 		catch(ValueNotFoundException e)
 		{ 
-			System.out.println("Please fill all values");
+			e.printStackTrace();
+		//	 System.out.println("Email invalid");
+			
 		//throw new ValueNotFoundException("Not found");
 		}
-	}
-	
-	
-	
-	
+	/*	catch(NullPointerException e)
+		{ 
+			e.printStackTrace();
+		//	System.out.println("Email invalid");
+			
+		//throw new ValueNotFoundException("Not found");
+		} */
+ 	}
+		
 }
+
